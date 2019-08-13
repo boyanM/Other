2033568de6c906636a7d23a0ecafde68adee9969
Boyan Milanov <33575839+boyanM@users.noreply.github.com>
2019-08-13 20:50:16 +0300

    Update filter.h

--- filter.h
+++ filter.h
@@ -1,8 +1,35 @@
 #ifndef __FILTER_H
 #define __FILTER_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+class Filter
+{
+private:
+	ifstream input;
+	fstream output;
+	string line;
+
+public:
+Filter();
+
+void filterSeparators();
+
+string checkForLeadingZeros(string line);
+
+string deleteFirstSeparators(string line);
 
 
+~Filter();
+	
+
+
+
+
+};
 
 
 
