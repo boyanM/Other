a8cfac8fbdcbea5e774789a4b1e17899ab94190b
Boyan Milanov <33575839+boyanM@users.noreply.github.com>
2019-08-13 20:50:40 +0300

    Create parse.cpp

---
+++ parse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "filter.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "filter.h"
+
+using namespace std;
+
+int main()
+{
+Filter parse;
+parse.filterSeparators();
+
+
+	
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+/*		Prints the file
+	while(getline(myfile,line))
+	{
+		cout<<line<<endl;
+	}
+*/	
